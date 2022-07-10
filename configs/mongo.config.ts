8feec6c907c96e32b4751495d30fc1cf5da6472c
Serj <sergey.dirheimer@gmail.com>
2022-07-11 01:23:24 +0300

    Project initialized

---
+++ configs/mongo.config.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (
+  configService: ConfigService,
+): Promise<TypegooseModuleOptions> => {
+  return {
+    uri: getMongoUri(configService),
+    ...getMongoOptions(),
+  };
+};
+
+const getMongoUri = (configService: ConfigService) =>
+  'mongodb://' +
+  configService.get('MONGO_LOGIN') +
+  ':' +
+  configService.get('MONGO_PASSWORD') +
+  '@' +
+  configService.get('MONGO_HOST') +
+  ':' +
+  configService.get('MONGO_PORT') +
+  '/';
+
+export const getMongoOptions = () => ({
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
